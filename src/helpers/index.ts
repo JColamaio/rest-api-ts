ff3700532d3d1d38ac0dda54797e4d9ad0f9c6e0
Jonathan <jonathan_colamaio@solutionbox.com.ar>
2023-03-27 12:26:58 -0300

    crypto and hash helper

---
+++ src/helpers/index.ts
@@ -0,0 +1,9 @@
+// auth helpers
+import crypto from 'crypto';
+const SECRET = process.env.SECRET
+
+export const randomizer = () => crypto.randomBytes(128).toString('base64');
+export const authentication = (salt: string, password: string) => {
+    return crypto.createHmac('sha256', [salt,password].join('/')).update(SECRET).digest('hex');
+};
+
